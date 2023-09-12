d672e36be029b5fbaaf595e3fec2201b81dd4939
PoiScript <poiscript@gmail.com>
2023-09-13 04:17:50 +0800

    disable sentry cli

--- web/cfpages_build.sh
+++ web/cfpages_build.sh
@@ -8,12 +8,3 @@ yarn ng run holostats:server:production
 yarn ng run holostats:build:production --stats-json=false
 
 node postbuild.mjs
-
-# create a deploy in sentry
-if [ "$CF_PAGES_BRANCH" = "master" ] || [ "$CF_PAGES_BRANCH" = "dev" ]; then
-  yarn global add @sentry/cli
-  sentry-cli releases new $CF_PAGES_COMMIT_SHA
-  sentry-cli releases set-commits $CF_PAGES_COMMIT_SHA --commit "PoiScript/HoloStats@$CF_PAGES_COMMIT_SHA"
-  sentry-cli releases deploys $CF_PAGES_COMMIT_SHA new -e $CF_PAGES_BRANCH
-  sentry-cli releases finalize $CF_PAGES_COMMIT_SHA
-fi
